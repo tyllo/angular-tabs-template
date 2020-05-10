7add821fbe041dd154d0489cd2047681b9de31c0
Andrey Pastukhov <gmtyllo@gmail.com>
2020-05-11 02:42:17 +1000

    Add timer to test.component

--- app/test.component.ts
+++ app/test.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'test',
   template: `
     <div>
-      TestComponent {{ this.tab }} content
+      TestComponent {{ this.tab }} content; timer: {{ timer }}
     </div>
   `,
 })
-export class TestComponent implements OnInit {
+export class TestComponent implements OnInit, OnDestroy {
+  private intervalId;
+
+  timer = 0;
+
   @Input() tab: number;
 
   public ngOnInit() {
+    this.intervalId = setInterval(() => this.timer++, 1000);
     console.log(`>>> TestComponent ${this.tab} initialized`);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
   }
 }
